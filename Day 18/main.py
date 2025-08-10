d0c8f62ed369d934b0776fa5f7588081c0aad661
Anas Kaarov <anasbinkurban@gmail.com>
2025-08-10 18:26:02 +0600

    [Day 18] 133. Turtle Challenge 4 - Generate a Random Walk

--- Day 18/main.py
+++ Day 18/main.py
@@ -7,15 +7,11 @@ colours = [
     "CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
     "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"
 ]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
 
-
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
-
-
-for shape_sides in range(3, 11):
+for _ in range(200):
     tim.color(random.choice(colours))
-    draw_shape(shape_sides)
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
