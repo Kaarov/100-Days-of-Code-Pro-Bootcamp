5bc8bd88a30baa0b3b271aafa256f862aafa8a16
Anas Kaarov <anasbinkurban@gmail.com>
2025-08-10 17:47:07 +0600

    [Day 18] 128. Understanding Turtle Graphics and How to use the Documentation

---
+++ Day 18/main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+
+
+screen = Screen()
+screen.exitonclick()
+
+# 128. Understanding Turtle Graphics and How to use the Documentation
