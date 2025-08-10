ebbf198efe6504050a557dd809c61f7a41ab830a
Anas Kaarov <anasbinkurban@gmail.com>
2025-08-10 20:57:07 +0600

    [Day 20] 148. Animating the Snake Segments on Screen

--- Day 20: Snake Game Part 1/main.py
+++ Day 20: Snake Game Part 1/main.py
@@ -1,15 +1,32 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
 
 for starting_position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(starting_position)
+    segments.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for segment_number in range(len(segments) - 1, 0, -1):
+        new_x = segments[segment_number - 1].xcor()
+        new_y = segments[segment_number - 1].ycor()
+        segments[segment_number].goto(new_x, new_y)
+    segments[0].forward(20)
 
 screen.exitonclick()
