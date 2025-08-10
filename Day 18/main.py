52509e506435a354277b3ea2a5f4af8513e119a4
Anas Kaarov <anasbinkurban@gmail.com>
2025-08-10 17:58:13 +0600

    [Day 18] 129. Turtle Challenge 1 - Draw a Square

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,12 +1,15 @@
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
 
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
 
 screen = Screen()
 screen.exitonclick()
-
-# 128. Understanding Turtle Graphics and How to use the Documentation
