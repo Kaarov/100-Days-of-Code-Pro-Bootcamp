d0b3165ccf380b44f08cefd1c64a8ca21b33aee8
Anas Kaarov <anasbinkurban@gmail.com>
2025-08-28 07:04:53 +0600

    [Day 27] 209. Buttons, Entry, and Setting Component Options

--- Day 27 - Intermediate - Tkinter, *args, **kwargs and Creating GUI Programs/main.py
+++ Day 27 - Intermediate - Tkinter, *args, **kwargs and Creating GUI Programs/main.py
@@ -1,11 +1,30 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+my_label["text"] = "New text"
+my_label.config(text="New text")
+
+
+# Button
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
 
 window.mainloop()
