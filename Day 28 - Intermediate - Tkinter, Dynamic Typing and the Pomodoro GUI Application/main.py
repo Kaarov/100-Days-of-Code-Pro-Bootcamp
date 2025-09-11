d845b18ebf5cd894927aa188eb5ef9e593ccde6c
Anas <anas_kaarov@epam.com>
2025-09-11 11:09:57 +0600

    [Day 28] 219. Adding Checkmarks and Resetting the Application

--- Day 28 - Intermediate - Tkinter, Dynamic Typing and the Pomodoro GUI Application/main.py
+++ Day 28 - Intermediate - Tkinter, Dynamic Typing and the Pomodoro GUI Application/main.py
@@ -11,11 +11,22 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+
 def start_timer():
     global reps
     reps += 1
@@ -36,6 +47,7 @@ def start_timer():
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
@@ -45,9 +57,15 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -68,7 +86,7 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-rest_button = Button(text="Reset", highlightthickness=0)
+rest_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 rest_button.grid(column=2, row=2)
 
 check_marks = Label(text="✓", fg=GREEN, bg=YELLOW, highlightthickness=0)
