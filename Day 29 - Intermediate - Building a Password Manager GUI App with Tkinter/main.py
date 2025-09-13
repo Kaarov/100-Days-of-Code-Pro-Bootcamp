ffb0bf25bb79835854f7ef4632d7b2743cf07c77
Anas <anas_kaarov@epam.com>
2025-09-13 17:42:12 +0600

    [Day 29] 223. Solution to the Creating the Grid Layout

--- Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/main.py
+++ Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/main.py
@@ -8,10 +8,32 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=1)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2)
+password_label = Label(text="password:")
+password_label.grid(row=3)
+
+# Entries
+website_label = Entry(width=35)
+website_label.grid(row=1, column=1, columnspan=2)
+email_label = Entry(width=35)
+email_label.grid(row=2, column=1, columnspan=2)
+password_label = Entry(width=21)
+password_label.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=4, column=2)
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
