2feed9139010017db00a55c58ae36fa9b0d754eb
Anas <anas_kaarov@epam.com>
2025-09-15 10:53:05 +0600

    [Day 30] 228. Catching Exceptions: The try catch except finally Pattern

---
+++ Day 30 - Intermediate - Errors, Exceptions and JSON Data Improving the Password/main.py
@@ -0,0 +1,17 @@
+# FileNotFound
+
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["aws"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"That key {error_message} doesn't exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
