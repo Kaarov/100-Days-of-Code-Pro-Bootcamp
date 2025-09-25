512dd999d8663e3e3e6a8da8229bbaf81ae0dddf
Anas Kaarov <anas@Anass-MacBook-Air.local>
2025-09-25 11:44:27 +0600

    [Day 32] 249. Solution & Walkthrough for the Automated Birthday Wisher

--- Day 32 - Intermediate+ Send Email (smtplib) & Manage Dates (datetime)/main.py
+++ Day 32 - Intermediate+ Send Email (smtplib) & Manage Dates (datetime)/main.py
@@ -1,46 +1,29 @@
-import smtplib
-import datetime as dt
+from datetime import datetime
+import pandas
 import random
+import smtplib
 
 MY_EMAIL = "email@gmail.com"
 MY_PASSWORD = "password"
 
-now = dt.datetime.now()
-weekday = now.weekday()
-if weekday == 1:
-    with open("quotes.txt") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-        print(quote)
-        with smtplib.SMTP('smtp.gmail.com') as connection:
-            connection.starttls()
-            connection.login(MY_EMAIL, MY_PASSWORD)
-            connection.sendmail(
-                from_addr=MY_EMAIL,
-                to_addrs=MY_EMAIL,
-                msg=f"Subject:Monday Motivation!\n\n{quote}"
-            )
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-# my_email = "email@gmail.com"
-# password = "password"
-#
-# with smtplib.SMTP('smtp.gmail.com') as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="guest@gmail.com",
-#         msg="Subject:Hello\n\nThis is the body of my email."
-#     )
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-import datetime as dt
-
-now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
-print(now)
-
-date_of_birth = dt.datetime(year=1995, month=12, day=15, hour=4)
-print(date_of_birth)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
