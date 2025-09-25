41458fafda436c0b79a088c1e9d2e1c5f34cfc53
Anas Kaarov <anas@Anass-MacBook-Air.local>
2025-09-25 11:20:50 +0600

    [Day 32] 246. Working with the datetime Module

--- Day 32 - Intermediate+ Send Email (smtplib) & Manage Dates (datetime)/main.py
+++ Day 32 - Intermediate+ Send Email (smtplib) & Manage Dates (datetime)/main.py
@@ -1,13 +1,24 @@
-import smtplib
+# import smtplib
+#
+# my_email = "email@gmail.com"
+# password = "password"
+#
+# with smtplib.SMTP('smtp.gmail.com') as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="guest@gmail.com",
+#         msg="Subject:Hello\n\nThis is the body of my email."
+#     )
 
-my_email = "email@gmail.com"
-password = "password"
+import datetime as dt
 
-with smtplib.SMTP('smtp.gmail.com') as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="guest@gmail.com",
-        msg="Subject:Hello\n\nThis is the body of my email."
-    )
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(now)
+
+date_of_birth = dt.datetime(year=1995, month=12, day=15, hour=4)
+print(date_of_birth)
